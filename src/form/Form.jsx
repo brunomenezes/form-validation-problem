5eda943653f6d702cedb53489f341738f305668f
Bruno Menezes <brunodmenezes@gmail.com>
2019-02-09 10:14:44 +1300

    added Form component and testing

---
+++ src/form/Form.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Form extends Component {
+    render() {
+        return (
+            <h1>Form goes here!</h1>
+        )
+    }
+}
---
+++ src/form/Form.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Form from './Form';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Form />, div);
+    ReactDOM.unmountComponentAtNode(div);    
+});
+
+it('should render a form element', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Form />, div);
+    expect(div.children.length).toBe(1);
+    expect(div.querySelector('form')).toBeTruthy();
+});
